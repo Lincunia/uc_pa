eb10ae7324de2c6aafb92094b0ec01dce404a5c0
Elias <Lincunia>
2023-04-08 17:23:44 -0500

    For my partner

--- c_and_cpp/distance_by_min/foo.c
+++ c_and_cpp/distance_by_min/foo.c
@@ -1,4 +1,3 @@
-#include <stdio.h>
 #include "header.h"
 
 unsigned long long int distance_by_min()
@@ -7,4 +6,20 @@ unsigned long long int distance_by_min()
     printf("Insert the amount of minutes you need to convert: ");
     scanf("%d", &m);
     return SPEED * 60 * m;
+}
+void in_out_chars(){
+	char c;
+	bool a=false;
+	printf("Insert any text you want: ");
+	while(c!='\n'){
+		c=getchar();
+		putchar(c);
+	}
+	printf("%d", a);
+}
+void clear_it(void){
+	int c;
+	do{
+		c=getchar();
+	}while(c!='\n' && c!=EOF);
 }
---
+++ c_and_cpp/practice/c.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <time.h>
+
+int return_month();
+
+int main ()
+{
+  unsigned int t;
+  printf("Inserte el total de la compra\n");
+  scanf("%ud", &t);
+  if (return_month()==10) {
+    printf("Total a pagar: %f\n", t*0.2);
+  }
+  printf("Total a pagar: %d\n", t);
+  return 0;
+}
+
+int return_month(){
+  time_t rawtime;
+  struct tm * timeinfo;
+
+  time ( &rawtime );
+  timeinfo = localtime ( &rawtime );
+  return timeinfo->tm_mon;
+}
