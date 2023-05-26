18b5a0915427dc914a3e7ae4bb6e20fed3c83566
Lincunia <pll52742@gmail.com>
2023-05-26 01:34:00 -0500

    upgrade_to_the project

--- c_and_cpp/pr_one/basic.h
+++ c_and_cpp/pr_one/basic.h
@@ -1,28 +1,31 @@
 #ifndef BEGGING
 #define BEGGING
-/*
- * Libreries
- */
 #include <iostream>
-#include <stdlib.h>
-#include <string.h>
-/*
- * Classes, unions, etc.
- */
-class user {
-public:
-  user(int, int, char *);
-  unsigned int id, money;
-  char name[100];
+#include <cstdlib>
+#include <cctype>
+using namespace std;
+// Basic matrix
+// Scrappy reference: Code-Description-Unities-Size of unities-prize
+// This thing will improve
+string productos[10][5]={
+  {"1", "Arroz", "Lb", "1", "2000"},
+  {"2", "Azucar morena", "Lb", "2", "5600"},
+  {"3", "Papa pastusa", "Lb", "4", "8900"},
+  {"4", "Zanahoria", "Lb", "2", "3700"},
+  {"5", "Sal", "Lb", "2", "2000"},
+  {"6", "Café", "g", "170", "22600"},
+  {"7", "Chocolate", "g", "500", "7300"},
+  {"8", "Avena", "g", "1200", "11700"},
+  {"9", "Pan tajado", "g", "600", "6000"},
+  {"10", "Agua", "ml", "6000", "6500"},
 };
-user::user(int in_id, int in_money, char *in_name) {
-  id=in_id;
-  strcpy(name, in_name);
-  money=in_money;
-}
-/*
- * Functions
- */
-void add_users();
-void print_file(char *);
+
+// These functions
+
+void clear_it();
+void pause();
+void client_time(int, int *);
+bool check_matrix(int *, string, bool);
+void print_piece();
+
 #endif // !BEGGING
--- c_and_cpp/pr_one/main.cpp
+++ c_and_cpp/pr_one/main.cpp
@@ -1,21 +1,108 @@
 #include "basic.h"
 
-int main(){
-  add_users();
-  print_file(txt);
+int main(void)
+{
+  char opt;
+  int clients=0, sales=0;
+  do
+  {
+    cout << "\nNúmero de ventas: " << sales
+      << "\nNúmero de clientes: " << clients
+      << "\n\n=============================\n\tMENÚ"
+      << "\n=============================\n"
+      << "\na. Salir"
+      << "\nb. Trabajar con otro cliente\n"
+      << "\nInserte la opción: ";
+    opt = getchar();
+    system("clear || cls");
+    clear_it();
+    switch (opt)
+    {
+      case 'a':
+        cout << "Gracias por usar el programa *U*";
+        break;
+
+      case 'b':
+        clients++;
+        client_time(clients, &sales);
+        break;
+
+      default:
+        cout << "Por favor, no inserte una opción externa a lo que necesita";
+        break;
+    }
+    cout << "\n";
+  } while (opt != 'a');
+  pause();
   return 0;
 }
 
-void add_users(){
-  std::cout << "Hola, subnormal" << std::endl;
+void clear_it()
+{
+  int c;
+  while ((c = getchar()) != '\n' && c != EOF);
 }
-void print_file(char *name){
-  FILE *p_file;
-  p_file=fopen("users.txt", "a");
-  if(p_file==NULL){
-    fputs("Error: ", stderr);
-    abort();
+void pause()
+{
+  cout << "Presione enter para continuar ...";
+  char enter = 0;
+  while( getchar() != '\n' );
+}
+bool check_matrix(int *it, string code, bool chk)
+{
+  for (*it = 0; *it < 10; *it+=1)
+  {
+    if(code==productos[*it][0]){
+      chk=true;
+      break;
+    }
   }
-  fprintf(p_file, "%s\n", name);
-  fclose(p_file);
+  return chk;
+}
+void client_time(int i, int *sales)
+{
+  string code;
+  char c_opt, *ft;
+  int subtotal=0, it, amount;
+  cout << "¡Muy buenos días clinete número " << i << "!\n";
+  do {
+    bool chk=false;
+    cout << "\nPor favor inserte el código del producto:\n";
+    getline(cin, code);
+    chk=check_matrix(&it, code, chk);
+    if(!chk){
+      system("clear || cls");
+      cout << "Lo sentimos, ese producto no se encuentra\n\n¿Quiere meter otro producto?\n";
+      continue;
+    }
+    cout << "\nInserte ahora, la cantidad de este:\n";
+    cin >> amount;
+    clear_it();
+    subtotal+=(strtol(productos[it][4].c_str(), &ft, 10)*amount);
+    cout << "Gracias por su compra, este es el monto total a pagar: " << subtotal << "\n\n";
+ask_it:
+    cout << "¿Desea continuar? ('Y' para si o 'N' para no):\n";
+    c_opt=getchar();
+    clear_it();
+    c_opt=toupper(c_opt);
+    if(c_opt!='N' && c_opt!='Y'){
+      cout << "Recuerde que tiene que escribir bien la opción\n\n";
+      goto ask_it;
+    }
+    *sales+=1;
+  } while(c_opt!='N');
+  print_piece();
+}
+void print_piece()
+{
+  system("clear || cls");
+  cout << "\n" << "\tEMPRESA: MERCADO DE U CENTRAL\n"
+    << "\tCarrera 5 n.ª21-38\n\n"
+    << "======COMPRAS TOTALES=====\n"
+    << "\nSubtotal:\t\t" << "Me importa un carajo"
+    << "\nImpuesto:\t\t" << "No voy a ser una rata con usted"
+    << "\n\nTotal a pagar:\t\t" << "Olvidate de esas pagas absurdas"
+    << "\n\n==========================\n\n";
+  pause();
+  system("clear || cls");
 }
