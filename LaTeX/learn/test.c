7fef5dd3dece9b9664a1aceaf550584fc8efc78f
Lincunia <pll52742@gmail.com>
2024-02-08 00:10:30 -0500

    rejection

--- LaTeX/learn/test.c
+++ LaTeX/learn/test.c
@@ -1,9 +1,8 @@
 #include <stdio.h>
-#include <stdlib.h>
 
 int main(int argc, char *argv[]) {
   for (int i = 1; i < argc; i++) {
     printf("%s\n", argv[i]);
   }
-  return EXIT_SUCCESS;
+  return 0;
 }
